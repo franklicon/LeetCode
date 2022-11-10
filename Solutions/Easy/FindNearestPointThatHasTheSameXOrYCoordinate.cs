1445fc92dcc826b448bec1b40f4ccb96a040724f
FranciscoLicon <francisco17@ciencias.unam.mx>
2022-11-10 13:37:51 -0600

    Add FindNearestPointThatHasTheSameXOrYCoordinate

---
+++ Solutions/Easy/FindNearestPointThatHasTheSameXOrYCoordinate.cs
@@ -0,0 +1,31 @@
+﻿namespace Solutions.Easy
+{
+    public static class FindNearestPointThatHasTheSameXOrYCoordinate
+    {
+        public static int Solution(int x, int y, int[][] points)
+        {
+            var n = points.Length - 1; 
+            var minDistance = int.MaxValue;
+            var index = -1;
+            for(var i = 0; i <= n; i++)
+            {
+                if (points[i][0] == x || points[i][1] == y)
+                {
+                    var manhattanDistance = ManhattanDistance(x, y, points[i][0], points[i][1]);
+                    if (manhattanDistance < minDistance)
+                    {
+                        minDistance = manhattanDistance;
+                        index = i;
+                    }
+                }
+            }
+
+            return index;
+        }
+
+        private static int ManhattanDistance(int x1, int y1, int x2, int y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+    }
+}
---
+++ Tests/Easy/FindNearestPointThatHasTheSameXOrYCoordinateTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Easy;
+
+namespace Tests.Easy
+{
+    [TestClass]
+    public class FindNearestPointThatHasTheSameXOrYCoordinateTest
+    {
+        [TestMethod]
+        public void Test01()
+        {
+            // Arrange
+            var points = new int[][] { 
+                new int[] { 1, 2 },
+                new int[] { 3, 1 },
+                new int[] { 2, 4 },
+                new int[] { 2, 3 },
+                new int[] { 4, 4 },
+            };
+            var x = 3;
+            var y = 4;
+            var expectedResult = 2;
+
+            // Act
+            var actualResult = FindNearestPointThatHasTheSameXOrYCoordinate.Solution(x, y, points);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
