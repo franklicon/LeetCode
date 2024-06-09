3a857f362e53d120ad56407538131379f6ef2e39
Francisco Licon <FranciscoArturo.Licon@Honeywell.com>
2024-06-08 18:26:06 -0600

    Add Container With Most Water

---
+++ Solutions/Medium/ContainerWithMostWater.cs
@@ -0,0 +1,29 @@
+﻿namespace Solutions.Medium
+{
+    public static class ContainerWithMostWater
+    {
+        public static int Solution(int[] height)
+        {
+            var a = 0;
+            var b = height.Length - 1;
+            var maxArea = 0;
+            while (a < b)
+            {
+                var area = Math.Min(height[a], height[b]) * (b - a);
+                if (area > maxArea)
+                {
+                    maxArea = area;
+                }
+                if(height[a] < height[b])
+                {
+                    a++;
+                }
+                else
+                {
+                    b--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
---
+++ Tests/Medium/ContainerWithMostWaterTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Medium;
+
+namespace LeetCode.Tests.Medium
+{
+    [TestClass]
+    public class ContainerWithMostWaterTest
+    {
+        [TestMethod]
+        public void Test01()
+        {
+            // Arrange
+            var height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            var expectedResult = 49;
+
+            // Act
+            var actualResult = ContainerWithMostWater.Solution(height);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            // Arrange
+            var height = new int[] { 1, 1 };
+            var expectedResult = 1;
+
+            // Act
+            var actualResult = ContainerWithMostWater.Solution(height);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            // Arrange
+            var height = new int[] { 4, 3, 2, 1, 4 };
+            var expectedResult = 16;
+
+            // Act
+            var actualResult = ContainerWithMostWater.Solution(height);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test04()
+        {
+            // Arrange
+            var height = new int[] { 1, 2, 1 };
+            var expectedResult = 2;
+
+            // Act
+            var actualResult = ContainerWithMostWater.Solution(height);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
