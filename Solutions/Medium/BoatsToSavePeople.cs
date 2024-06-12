36140eceaa646b3f8edb124ab6b1d7228211132f
Francisco Licon <FranciscoArturo.Licon@Honeywell.com>
2024-06-12 11:10:45 -0600

    Add Boats to Save People

---
+++ Solutions/Medium/BoatsToSavePeople.cs
@@ -0,0 +1,28 @@
+﻿namespace Solutions.Medium
+{
+    public static class BoatsToSavePeople
+    {   
+        public static int Solution(int[] people, int limit)
+        {
+            Array.Sort(people);
+            var left = 0;
+            var right = people.Length - 1;
+            var boats = 0;
+            while (left <= right)
+            {
+                if (people[left] + people[right] <= limit)
+                {
+  
+                    left++;
+                    right--;
+                }
+                else
+                {
+                    right--;
+                }
+                boats++;
+            }
+            return boats;
+        }
+    }
+}
---
+++ Tests/Medium/BoatsToSavePeopleTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Medium;
+
+namespace LeetCode.Tests.Medium
+{
+    [TestClass]
+    public class BoatsToSavePeopleTest
+    {
+        [TestMethod]
+        public void Test01()
+        {
+            // Arrange
+            var people = new int[] { 1, 2 };
+            var limit = 3;
+            var expectedResult = 1;
+
+            // Act
+            var actualResult = BoatsToSavePeople.Solution(people, limit);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            // Arrange
+            var people = new int[] { 3, 2, 2, 1 };
+            var limit = 3;
+            var expectedResult = 3;
+
+            // Act
+            var actualResult = BoatsToSavePeople.Solution(people, limit);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
